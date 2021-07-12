6bb604a4483cb3e2edfbe31dbb991566b72fd307
Connector403 <hafiz-2010@hotmail.co.uk>
2021-07-12 13:20:33 +0100

    fixed contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -11,5 +11,5 @@
     $txt = "Youyy have received an e-mail from ". $name. ".\n\n".$message;
 
     mail($mailTo , $subject, $txt, $headers);
-    header("Location: index.html?mailsend");
+    header("Location: ../../index.html?mailsend");
   }
