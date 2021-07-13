f9f198f17cd77b7135a37b55ecc41c4183d0e41a
Connector403 <hafiz-2010@hotmail.co.uk>
2021-07-13 18:38:28 +0100

    happy

--- forms/contact.php
+++ forms/contact.php
@@ -17,7 +17,7 @@ if (isset($_POST['submit'])) {
 
     //   echo  '<script> alert("Thank you, Your message was send successfully!")</script>';
     echo "<script>
-            alert('Message  sent successfully');
+            document.getElementById('succ').html('Message Sent Successfully!');
             window.location.href='../../index.html';
             </script>";
   } else {
