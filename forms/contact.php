3d7e50da35b0e832cc3a30652969152be68dc7fb
Connector403 <hafiz-2010@hotmail.co.uk>
2021-07-13 14:19:45 +0100

    contact entry

--- forms/contact.php
+++ forms/contact.php
@@ -10,7 +10,12 @@
     $headers = "From: ". $mailFrom;
     $txt = "You have received an e-mail from ". $name. ".\n\n".$message;
 
-    mail($mailTo , $subject, $txt, $headers);
-    header("Location: ../../index.html?mailsend");
-    $_SESSION['sent-message']='Thank you, Youre message have been Sent!';
+    if(  mail($mailTo , $subject, $txt, $headers)){
+      echo '<div class="sent-message"> Your message has been sent. Thank you!</div>';
+    } else {
+      echo '  <div class="error-message">Failure to send message</div> ';
+    }
+  
+    // header("Location: ../../index.html?mailsend");
+    // $_SESSION['sent-message']='Thank you, Youre message have been Sent!';
   }
